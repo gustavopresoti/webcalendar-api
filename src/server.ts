3a0b53615e14d83b53b93425522939ca3a20d811
gustavo.presoti <gu.presoti@gmail.com>
2021-10-04 11:40:27 -0300

    add: mongo connection and server options

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import routes from './routes';
 
 const app = express()
 
-app.get('/', (request: Request, response: Response) => {
-  return response.json({ message: 'Welcome to WebCalendar API' })
-})
+const mongoUser = process.env.MONGO_USER
+const mongoPassword = process.env.MONGO_PASSWORD
+
+mongoose.connect(`mongodb+srv://${mongoUser}:${mongoPassword}@clusterwebcalendar.2zzh5.mongodb.net/webcalendar?retryWrites=true&w=majority`);
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 app.listen(3333)
